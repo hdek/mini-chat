0d1844dd41fd990af2b999c6592e978fb90df734
hdek <hdek@gmail.com>
2016-03-10 20:49:07 +0100

    Modification du fichier index.php : ajout de la liste des 10 derniers messages en dessous du formulaire.

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  // Connexion à la base de données
+  include 'connect.php';
+?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -44,6 +48,29 @@
         </div>
       </div>
 
+      <div class="panel panel-success">
+        <div class="panel-heading">
+          <h3 class="panel-title">Les 10 derniers messages</h3>
+        </div>
+        <div class="panel-body">
+<?php
+  // Récupération des 10 derniers messages
+  $reponse = $bdd->query('SELECT U.pseudo, DATE_FORMAT(M.cree_le, \'%d/%m/%Y %H:%i:%s\') AS cree_le, M.message '.
+                         'FROM minichat M '.
+                         'INNER JOIN utilisateur U ON M.id_pseudo = U.id '.
+                         'ORDER BY M.cree_le DESC '.
+                         'LIMIT 0, 10');
+
+  // Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+  while ($donnees = $reponse->fetch()) {
+    // je ne met pas la fonction htmlspecialchars pour la date parce que la date n'est pas reseigné par l'utilisateur
+    echo '<p>' . $donnees['cree_le'] .' <strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+  }
+
+  $reponse->closeCursor();
+?>
+        </div>
+      </div>
     </div>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
