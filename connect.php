5926fa01aac1873cc93b885cd07296b7923b1f8d
hdek <hdek@gmail.com>
2016-03-10 19:42:23 +0100

    Ajout du fichier connect.php pour la connection à la base de données

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+  // Connexion à la base de données
+  $base = 'mini_chat';
+  $user = 'test';
+  $password = 'test';
+
+  try {
+    $bdd = new PDO('mysql:host=localhost;dbname=' . $base . ';charset=utf8', $user, $password);
+  }
+  catch(Exception $e) {
+    die('Erreur : '.$e->getMessage());
+  }
+?>
